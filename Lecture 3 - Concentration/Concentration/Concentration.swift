e88bd22a7111dfcd56486057f54e9bc999bc885a
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-01-15 15:41:08 +0700

    Lecture 3 - Concentration

---
+++ Lecture 3 - Concentration/Concentration/Concentration.swift
@@ -0,0 +1,58 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Konstantin Kotsarev on 14.01.2022.
+//
+
+import Foundation
+
+class Concentration
+{
+    private(set) var cards = [Card]()
+    
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+    
+    func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            } else {
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of cards")
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle the cards
+    }
+}
