8ef67fb6858f464e4c471cd92fb4e0e01015ae64
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-01-22 13:35:26 +0700

    Lecture 9 - Cassini

---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Konstantin Kotsarev on 22.01.2022.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate
+{
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+
+    @IBOutlet weak var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    var imageView = UIImageView()
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
+}
---
+++ Cassini/Cassini/Supporting Files/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Cassini
+//
+//  Created by Konstantin Kotsarev on 22.01.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
