47f036d8df668f7ede756184546ad32a5f90862e
Konstantin Kotsarev <kotsaarev@gmail.com>
2022-02-01 16:19:26 +0700

    Lecture 17 - PlayingCard

---
+++ PlayingCard/PlayingCard/CMMotionManager+shared.swift
@@ -0,0 +1,12 @@
+//
+//  CMMotionManager+shared.swift
+//  PlayingCard
+//
+//  Created by Konstantin Kotsarev on 01.02.2022.
+//
+
+import CoreMotion
+
+extension CMMotionManager {
+    static var shared = CMMotionManager()
+}
